abdfbb9641e8bfd234083d75845bd0f8980d0ccb
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-01-06 11:34:27 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+const electron = require('electron');
+const fetch = require("node-fetch");
+
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
+
+
+
+
+
+  
